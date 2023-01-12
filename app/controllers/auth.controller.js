dd54a673e9e15c31240c97106619eaafe7028662
dongle47 <dong3601927@gmail.com>
2023-01-12 03:26:53 +0000

    modify birthday

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -11,7 +11,7 @@ exports.signup = (req, res) => {
     password: bcrypt.hashSync(req.body.password, 8),
     avatar: "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=",
     fullName: "",
-    birthDay: null,
+    birthDay: "",
     sex: "",
     email: ""
   });
